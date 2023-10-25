95b64a706b7fafd09f3e677aec033385495edc03
Ste <steshaw19@gmail.com>
2023-10-25 17:16:00 +0100

    Adds Table of contents to README file and asks user input and checks validity in milestone_2 python file.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Hangman-Project-AiCore
+## Table of Contents
+- Project description
+- Installation instructions
+- Usage instructions
+- File structure of the project
+- License information
+## Project Description
+This project is inspired by the classic Hangman game. 
--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,11 @@
+import random
+
 word_list = ["apple", "banana", "orange", "blueberry", "fig"]
-print(word_list)
+word = random.choice(word_list)
+
+guess = input("Please choose a letter: ")
+while guess.isalpha() == False or len(guess) != 1:
+    print("Oops! That is not a valid input.")
+    guess = input("Please choose a letter: ")
+else:
+    print("Good guess!")
