b9168f2925c085fc58c05325387b8833b4dbe2d0
Ste <steshaw19@gmail.com>
2023-10-27 09:55:31 +0100

    Git branch feature edits milestone 3.

--- milestone_3.py
+++ milestone_3.py
@@ -7,16 +7,27 @@ word_list = ["apple", "banana", "orange", "blueberry", "fig"]
 word = random.choice(word_list)
 
 ## Takes user input and checks if it is a single letter and is alphabetical.
-guess = input("Please choose a letter: ")
+## guess = input("Please choose a letter: ")
+
+##while True:
+   ## if guess in word: 
+       ## print("Good guess - that letter is in the word!\n")
+        ##guess = input("Please choose your next letter: ")
+    ##elif guess not in word:
+       ## print("That's a good guess, but not in this word.\n")
+        ##guess = input("Please choose your next letter: ")
+    ##else:
+       ## if guess.isalpha() == False or len(guess) != 1:
+          ##  print("Oops! That is not a valid input.\n")
+           ## guess = input("Please choose a letter: ")
 
 while True:
-    if guess in word: 
-        print("Good guess - that letter is in the word!\n")
-        guess = input("Please choose your next letter: ")
-    elif guess not in word:
-        print("That's a good guess, but not in this word.\n")
-        guess = input("Please choose your next letter: ")
+    guess = input("Please choose a letter: ")
+    if guess.isalpha() == False or len(guess) != 1:
+        print("Invalid letter. Please, enter a single alphabetical character.")
     else:
-        if guess.isalpha() == False or len(guess) != 1:
-            print("Oops! That is not a valid input.\n")
-            guess = input("Please choose a letter: ")
+        if guess in word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+	
